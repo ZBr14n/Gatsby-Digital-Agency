ef732280b3d0392e48dfdc0641f6dc7d764fe975
Brian Lam <blam9459@gmail.com>
2020-06-01 11:43:49 -0400

    applied aos library

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,37 @@
 import React from "react"
 import {Reveal, Tween} from 'react-gsap'
-import {Animated} from "react-animated-css";
+import {Animated} from "react-animated-css"
 import '../styles/IndexPage.css'
+import '../styles/IndexPage2.css'
+
+import { Controller, Scene } from 'react-scrollmagic'
+import Aos from 'aos'
+import "aos/dist/aos.css"
+
+// import images
+import workplace1 from '../images/office_pics/group-of-people-sitting-indoors-3184291.jpg'
+import workplace2 from '../images/office_pics/people-sitting-beside-brown-wooden-desk-near-flat-screen-tv-1181355.jpg'
+import workplace3 from '../images/office_pics/photo-of-man-holding-a-book-927022.jpg'
+import workplace4 from '../images/office_pics/thoughtful-young-bearded-manager-typing-on-keyboard-while-3966786.jpg'
+import workplace5 from '../images/office_pics/top-view-of-people-at-the-meeting-3184287.jpg'
+import workplace6 from '../images/office_pics/woman-in-gray-jacket-sitting-beside-desk-2682452.jpg'
+import workplace7 from '../images/office_pics/woman-in-gray-sweater-drinking-coffee-3759089.jpg'
+
+
 
 const IndexPage = () => {
-
+  React.useEffect(()=>{
+    Aos.init({duration: 2000})
+  },[])
 
   return(
 
     <>
       <section className="sect-1">
+
+      
         
         <img className="logo" src="images/we-create.png" width="90px" />
-
 
         <div className="caption1">
             <div className="header-wrapper">
@@ -46,24 +65,31 @@ const IndexPage = () => {
       </Reveal>
 
 
-      <Reveal threshold={.1}>
+      <section className="sect-3-wrapper">
+            <div aos-data="fade-right" className="sect-3"></div>
+
+       </section>
+      {/* <Reveal threshold={.1}>
         <Tween from={{x:'90px'}} duration={5} ease="elastic.out(3,.6)">
           <section className="sect-3-wrapper">
             <div className="sect-3"></div>
 
           </section>
         </Tween>
-      </Reveal>
+      </Reveal> */}
 
-      
-      <Reveal threshold={.1}>
+
+          <section className="sect-4-wrapper">
+            <div data-aos="fade-left" className="sect-4"></div>
+          </section>      
+      {/* <Reveal threshold={.1}>
         <Tween from={{x:'90px'}} duration={5} ease="bounce.out">
 
           <section className="sect-4-wrapper">
             <div className="sect-4"></div>
           </section>
         </Tween>
-      </Reveal>
+      </Reveal> */}
 
 
       <br />
@@ -104,18 +130,29 @@ const IndexPage = () => {
             </div>
 
 
-            <div className="about-btn">
-              <div className="about-wrapper">
-                  About IMARC
-              </div>
+            <div data-aos="fade-up" data-aos-once="true" className="about-btn">
+                  <div className="about-wrapper">
+                      About IMARC
+                  </div>
             </div>
         </section>
 
       <br /><br /><br /><br /><br />
 
 
+      
+      
+      <section className="collage-container">
+        <section className="collage-wrapper">
+            <div className="test123">test</div>
 
+            <img src={workplace1} height="auto" alt="office" />
+            <img src={workplace2} height="auto" alt="office" />
+            <img src={workplace3} height="auto" alt="office" />
+            <img src={workplace4} height="auto" alt="office" />
 
+        </section>
+      </section>
 
 
 
